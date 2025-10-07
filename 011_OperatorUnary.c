8a86072d64abd70bd1deb21c331ba70f04d279fe
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-10-07 19:16:58 +0200

    Add files via upload

---
+++ 011_OperatorUnary.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a = 5;
+    int b = -a;        
+    
+    int c = 4;
+    int d = ++c;
+    
+    int e = 0;
+    int f = !a;        
+    
+    printf("b = %d\n", b);
+    printf("d = %d\n", d);
+    printf("f = %d\n", f);
+    return 0;
+}
+
