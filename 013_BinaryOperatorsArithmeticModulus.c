5df68605bd9ec804e9b708487acdde64c4c96d57
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-10-08 19:20:12 +0200

    Add files via upload

---
+++ 013_BinaryOperatorsArithmeticModulus.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a = 9;
+    int b = 3;
+    int res = a % b;
+
+    printf("El residuo de %d dividido por %d es: %d\n", a, b, res);
+    return 0;
+}
+
