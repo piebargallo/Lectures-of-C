25b67bdc7824797277582bcd34237f02b2643f44
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-10-11 11:56:58 +0200

    Add files via upload

---
+++ 019_RelationalOperadors.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() 
+{
+    int a = 0; 
+    int b = 1;
+    int c = 2;
+
+    printf("Resultado de a+b < c+d %d", a + b < b + c);
+    return 0;
+}
+
+// Output true
